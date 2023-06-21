161615d836a3241a9fcd3d5f743dd59ab061d69d
Rakesh Kanneeswaran <114800228+rakeshkanneeswaranofficial@users.noreply.github.com>
2023-06-21 15:39:46 +0530

    Add files via upload

---
+++ Model/Test/Test/applicationData.swift
@@ -0,0 +1,15 @@
+//
+//  applicationData.swift
+//  Test
+//
+//  Created by Apple on 21/06/23.
+//
+
+import Foundation
+import SwiftUI
+
+class applicationData : ObservableObject {
+    @Published var title : String = " Default String"
+    @Published var titleinput : String = ""
+    
+}
